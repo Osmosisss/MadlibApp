038264cc983841d6bd62aa3ccf2b92b8ed331fb0
Mac <mac@MacBook-Pro.local>
2018-05-10 11:37:15 -0600

    added header and header styles

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,14 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>DevCamp React Starter</h1>
+        <div className="backgroundSkew">
+          <div className="madlib-heading">
+            <h1>Osmosis Mad Libs</h1>
+            <div className="madlib-subheading">
+            FIll out the fields below and click the generate button<br/>to see Mad Lib story.
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
