b56b25082cdd4391ee15cfb98a3c2c55b4b9afe2
Mac <mac@MacBook-Pro.local>
2018-05-10 11:51:34 -0600

    created madlib form component

--- src/components/app.js
+++ src/components/app.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
+import MadlibForm from './madlib_form';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <div className="backgroundSkew">
+
           <div className="madlib-heading">
             <h1>Osmosis Mad Libs</h1>
             <div className="madlib-subheading">
             FIll out the fields below and click the generate button<br/>to see Mad Lib story.
             </div>
           </div>
+          <MadlibForm />
         </div>
       </div>
     );
